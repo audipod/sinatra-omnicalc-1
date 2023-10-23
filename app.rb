7c59dc2a9fc830e035f565f6155ebad018a532f7
audipod <146144639+audipod@users.noreply.github.com>
2023-10-23 17:51:13 +0000

    first commit

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+
+  @the_num = params.fetch("user_number").to_f
+  @the_result = params.fetch("user_number").to_f * params.fetch("user_number").to_f
+
+  erb(:square_results)
+
+end
+
+get("/square/root") do
+  erb(:new_root_calc)
+end
+
